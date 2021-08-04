14d34102ef49747064d578ac8613bfa62b2b56a0
Nemes1sX <kazstonk@gmail.com>
2021-08-04 15:36:05 +0300

    Add project files.

---
+++ CoinBase/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoinBase.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
