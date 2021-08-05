977f242fb855b05fb56409af4e791ce42c4b4cb0
Nemes1sX <kazstonk@gmail.com>
2021-08-05 14:48:22 +0300

    Vue on CDN finally intergrated

---
+++ CoinBase/wwwroot/js/Pages/Index.vue.js
@@ -0,0 +1,38 @@
+﻿var Index = {
+    template: `<div>
+                <h2>Bitcoin prices in EUR and USD</h2>
+                <div class="row">
+                    <div v-for="rate in rates">
+                        <div v-if="rate == 'USD'">
+                            <div class="md-6">
+                               <h4>USD: {{ rate }}</h4> 
+                            </div>
+                        </div>
+                        <div v-else-if="rate == 'EUR'">
+                            <div class="md-6">
+                               <h4>EUR: {{ rate }}</h4> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               </div> `,
+
+
+    data() {
+        return {
+            rates: "",
+        }
+    },
+
+
+    mounted() {
+        this.fetchBtcData()
+    },
+    methods: {
+        async fetchBtcData() {
+            const response = await axios("");
+        }
+    }
+
+
+};
--- CoinBase/wwwroot/js/app.js
+++ CoinBase/wwwroot/js/app.js
@@ -1,4 +1,20 @@
-﻿
-var app = new Vue({
-    el: "#app"
+﻿const routes = [
+    {
+        path: "/",
+        component: Index,
+    },
+];
+
+
+const router = new VueRouter({
+    routes: routes,
+    mode: "history",
+    base: "/",
 });
+
+var app = new Vue({
+    el: "#app",
+    router: router,
+});
+
+
